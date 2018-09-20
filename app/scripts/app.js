f968ffce2dc615824e8a2f8ba4b997f0a5d2a37b
rabp_ <rabp_@192.168.1.9>
2018-09-20 00:33:15 -0500

    Primer commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name proagrocorpFrontendApp
+ * @description
+ * # proagrocorpFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('proagrocorpFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proagrocorpFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the proagrocorpFrontendApp
+ */
+angular.module('proagrocorpFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
