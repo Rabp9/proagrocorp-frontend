f9aac9c86939dfb2af7caf369d429baad52e0e02
tmtrbocanegra <tmtrbocanegra@SGGT-PC.TMT.LOCAL>
2018-11-02 15:50:02 -0500

    Avanzando

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('proagrocorpFrontendApp')
 .controller('MainCtrl', function ($scope, $rootScope, $sce, ngProgressFactory,
-    slidesService, $q, imgResponsiveFilter) {
+    slidesService, $q, imgResponsiveFilter, categoriesService) {
     
     $scope.myInterval = 4000;
     $scope.noWrapSlides = false;
@@ -19,9 +19,27 @@ angular.module('proagrocorpFrontendApp')
         $scope.progressbar = ngProgressFactory.createInstance();
         $scope.progressbar.start();
         return $q.all([
-            slidesService.get().$promise
+            slidesService.get().$promise,
+            categoriesService.get({estado_id: 1}).$promise
         ]).then(function(data) {
             $scope.slides = data[0].slides;
+            $scope.categories = data[1].categories;
+            for (var i = 0; i < $scope.categories.length; i++) {
+                $scope.categories[i].col = 'col-sm-6';
+                if (i % 2) {
+                    $scope.categories[i].lp = "R";
+                } else {
+                    $scope.categories[i].lp = "L";
+                }
+                if (i > 1) {
+                    $scope.categories[i].lp += "D";
+                }
+            }
+            if ($scope.categories.length % 2) {
+                $scope.categories[$scope.categories.length - 1].col = 'col-sm-12';
+                $scope.categories[$scope.categories.length - 1].lp = 'LRD';
+            };
+            
             $scope.progressbar.complete();
         });
     };
@@ -48,6 +66,11 @@ angular.module('proagrocorpFrontendApp')
     
     $scope.getSlideSrc = function(slide, size) {
         var src = $rootScope.pathLocation + 'img/slides/' + slide.imagen;
+        return $sce.trustAsResourceUrl(imgResponsiveFilter(src, size));
+    };
+    
+    $scope.getBgSrc = function(category, size) {
+        var src = $rootScope.pathLocation + 'img/categories/' + category.portada;
         return $sce.trustAsResourceUrl(imgResponsiveFilter(src, size));
     };
     
