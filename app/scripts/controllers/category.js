d057c5be35f36a856a1bfc370c2ad8dfb4236161
rabp_ <rabp_@192.168.1.9>
2018-11-10 20:35:54 -0500

    2-17

--- app/scripts/controllers/category.js
+++ app/scripts/controllers/category.js
@@ -8,14 +8,24 @@
  * Controller of the proagrocorpFrontendApp
  */
 angular.module('proagrocorpFrontendApp')
-.controller('CategoryCtrl', function ($scope, $state, categoriesService) {
+.controller('CategoryCtrl', function ($scope, $state, categoriesService, $sce, $rootScope,
+    imgResponsiveFilter, ngProgressFactory) {
     
     $scope.init = function() {
+        $scope.progressbar = ngProgressFactory.createInstance();
+        $scope.progressbar.start();
         $scope.loading = true;
         categoriesService.get({id: $state.params.category_id}, function(data) {
             $scope.category = data.category;
             $scope.loading = false;
+            $rootScope.title = data.category.descripcion;
+            $scope.progressbar.complete();
         });
+    };
+    
+    $scope.getCategoriesSrc = function(category, size) {
+        var src = $rootScope.pathLocation + 'img/categories/' + category.portada;
+        return $sce.trustAsResourceUrl(imgResponsiveFilter(src, size));
     };
     
     $scope.init();
